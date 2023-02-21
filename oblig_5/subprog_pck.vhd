ecfb4e1f786eb64afbf1a9055b757448121c1ba1
Gregz9 <gregorkajda@gmail.com>
2023-02-21 13:43:49 +0100

    Assignment finished
    Signed-off-by: Gregz9 <gregorkajda@gmail.com>

--- oblig_5/subprog_pck.vhd
+++ oblig_5/subprog_pck.vhd
@@ -8,6 +8,9 @@ package subprog_pck is
   function xor_par(indata2: unsigned) return std_logic;
 
   function toggle_par(indata1: std_logic_vector) return std_logic;
+  
+  procedure test_pargen(signal clk : in std_logic; signal my_data : out unsigned); 
+
 
 end package subprog_pck;
 
@@ -29,6 +32,15 @@ package body subprog_pck is
   begin
       xor_pa := xor(indata2);
       return std_logic(xor_pa); 
+  end; 
+ 
+  procedure test_pargen(signal clk : in std_logic; signal my_data : out unsigned) is  
+  
+  begin
+    for i in 0 to 255 loop
+        my_data <= to_unsigned(i, my_data'length);
+        wait until rising_edge(clk); 
+      end loop; 
   end; 
 
 end package body subprog_pck;
