07ae4daf51a89d79b8899eff041aed276f51231d
Gregz9 <gregorkajda@gmail.com>
2023-04-04 15:28:28 +0200

    pulse width modulator finished
    Signed-off-by: Gregz9 <gregorkajda@gmail.com>

--- oblig_8/PWM_module.vhd
+++ oblig_8/PWM_module.vhd
@@ -4,84 +4,88 @@ use IEEE.numeric_std.all;
 use IEEE.float_pkg.all; 
 
 entity pulse_width_modulator is 
-  generic( 
-          PWM_frequency: natural := 2000 -- in Hz
-          ); 
 
   port( 
-      mclk       : in std_logic; 
+      mclk      : in std_logic; 
       reset     : in std_logic; 
       duty_cycle: in std_logic_vector(7 downto 0); 
-      dir       : out std_logic := '0';
-      en        : out std_logic := '1'
+      dir       : out std_logic;
+      en        : out std_logic 
     ); 
 end pulse_width_modulator; 
 
 architecture rtl of pulse_width_modulator is 
   
-  constant PWM_count : integer := 100000000 / PWM_frequency; 
-  constant max_value : integer := 128; 
-  
-  signal PWM_high : integer := 0; 
-  signal count_reg : integer := 0; 
-  signal count_next: integer := 0; 
-  signal new_dir : std_logic; 
+  type state_type is (reverse_idle, forward_idle, reverse, forward);
+  signal current_state, next_state : state_type;
+
+  signal count_reg, count_next : unsigned(15 downto 0) := (others => '0');  
+  signal PWM : std_logic := '0';
 
 begin 
 
   COUNTING: 
   process(count_reg) 
-  begin 
+  begin
     count_next <= count_reg + 1; 
   end process COUNTING; 
 
-
-  DIRECTION:
-  process(duty_cycle, mclk)
-  begin 
-    
-    if to_integer(signed(duty_cycle)) > 0 then 
-      PWM_high <= (to_integer(unsigned(duty_cycle))/max_value)*PWM_count;
-      new_dir <= '1'; 
-    else 
-      PWM_high <= abs(to_integer(unsigned(duty_cycle))/max_value)*PWM_count;
-      new_dir <= '0';
-    end if;
-
-    if reset = '1' then 
-      en <= '0';
-      dir <= '0';
-
-    elsif rising_edge(mclk) then 
-      if dir /= new_dir and en = '1' then  
-        en <= '0';
-      elsif dir /= new_dir and en = '0' then 
-        dir <= new_dir;
-      elsif dir = new_dir and en = '0' then 
-        en <= '1'; 
-      end if; 
-      
-
-    end if; 
-  end process DIRECTION; 
-  
-
-  REGISTERS: 
   process(mclk, reset) 
-  begin 
-    if reset = '1' then 
-      count_reg <= 0; 
-    elsif count_next > PWM_count then 
-      count_reg <= 0; 
+  begin
+    if reset then 
+      current_state <= reverse_idle;
+      count_reg <= (others => '0');
     elsif rising_edge(mclk) then 
-      if count_next <= PWM_high then 
-        count_reg <= count_next;
-      else 
-        count_reg <= count_next; 
-      end if; 
+      count_reg <= count_next;
+      current_state <= next_state;
     end if; 
-  end process REGISTERS; 
+  end process;
 
+  SET_PULSE: 
+  process(mclk)
+  begin 
+    if rising_edge(mclk) then 
+      if abs(signed(duty_cycle)) > abs(signed('0' & count_reg(15 downto 9))) then 
+        PWM <= '1'; 
+      else 
+        PWM <= '0';
+      end if; 
+    end if;
+  end process SET_PULSE;
+
+  NEXT_STATE_CL: 
+  process(all)  
+  begin 
+    case current_state is 
+      when reverse_idle => 
+          next_state <= reverse when signed(duty_cycle) < 0 else forward_idle;
+      when reverse => 
+          next_state <= reverse when signed(duty_cycle) < 0 else reverse_idle;
+      when forward_idle => 
+          next_state <= forward when signed(duty_cycle) > 0 else reverse_idle;
+      when forward => 
+          next_state <= forward when signed(duty_cycle) > 0 else forward_idle;
+      end case;
+    end process NEXT_STATE_CL;
+
+  OUTPUT_CL:
+  process(all) 
+  begin
+      case current_state is 
+        when reverse_idle => 
+          en <= '0';
+          dir <= '0';
+        when reverse => 
+          en <= PWM;
+          dir <= '0';
+        when forward_idle => 
+          en <= '0';
+          dir <= '1'; 
+        when forward => 
+          en <= PWM;
+          dir <= '1';
+        end case;
+  end process OUTPUT_CL; 
 end architecture;
 
 
