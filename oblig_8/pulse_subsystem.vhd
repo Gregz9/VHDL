50aeeca39ff32c941ae7342c5cb208f26230c014
Gregz9 <gregorkajda@gmail.com>
2023-04-06 17:44:25 +0200

    finished pulse subsystem
    Signed-off-by: Gregz9 <gregorkajda@gmail.com>

--- oblig_8/pulse_subsystem.vhd
+++ oblig_8/pulse_subsystem.vhd
@@ -32,4 +32,49 @@ architecture structural of pulse_subsystem is
     );
   end component pulse_width_modulator;
 
+  component output_synchronizer
+    port( 
+      clk       : in std_logic; 
+      reset     : in std_logic; 
+      dir       : in std_logic; 
+      en        : in std_logic; 
+      dir_synch : out std_logic; 
+      en_synch  : out std_logic 
+    );
+  end component output_synchronizer;
+
+  signal sub_duty_cycle : std_logic_vector(7 downto 0);
+  signal sub_dir : std_logic; 
+  signal sub_en  : std_logic; 
+
+begin 
+
+  SLFT : self_test_module
+  port map(
+        mclk => clk, 
+        reset => reset, 
+        duty_cycle => sub_duty_cycle
+      ); 
+
+  PWM : pulse_width_modulator
+  port map( 
+        mclk => clk, 
+        reset => reset, 
+        duty_cycle => sub_duty_cycle, 
+        dir => sub_dir, 
+        en => sub_en
+      ); 
+
+  SYNCH : output_synchronizer
+  port map( 
+        clk => clk, 
+        reset => reset, 
+        dir => sub_dir, 
+        en => sub_en, 
+        dir_synch => dir_synch, 
+        en_synch => en_synch
+      );
+
+end architecture structural;
+
 
