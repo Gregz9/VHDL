d844539dd5f120f923996df49c09d0aef85933ba
Gregz9 <gregorkajda@gmail.com>
2023-04-06 14:18:44 +0200

    Final version
    Signed-off-by: Gregz9 <gregorkajda@gmail.com>

--- oblig_8/quadrature_decoder.vhd
+++ oblig_8/quadrature_decoder.vhd
@@ -30,16 +30,21 @@ begin
     end if; 
   end process; 
 
+  ASSIGN_VALUES: process(clk, reset) 
+  begin 
+    if rising_edge(clk) then 
+      pos_inc <= inc; 
+      pos_dec <= dec;
+    end if; 
+  end process ASSIGN_VALUES;
+
+
   NEXT_STATE_CL: process(all) 
   begin 
     case current_state is 
       when S_reset => 
-        inc <= '0';
-        dec <= '0';
         next_state <= S_init;
       when S_init => 
-        inc <= '0';
-        dec <= '0';
         if synch_a & synch_b = "00" then 
             next_state <= S_0; 
         elsif synch_a & synch_b = "01" then 
@@ -52,68 +57,51 @@ begin
       when S_0 => 
         if synch_a & synch_b = "00" then 
             next_state <= S_0; 
-            inc <= '0';
-            dec <= '0';
         elsif synch_a & synch_b = "01" then 
             next_state <= S_1; -- inc
-            inc <= '1';
         elsif synch_a & synch_b = "11" then 
             next_state <= S_reset; -- err
-            err <= '1';
         elsif synch_a & synch_b = "10" then  
             next_state <= S_3; -- dec
-            dec <= '1';
         end if;
       when S_1 => 
         if synch_a & synch_b = "00" then 
             next_state <= S_0; -- dec 
-            dec <= '1';
         elsif synch_a & synch_b = "01" then 
             next_state <= S_1; 
-            inc <= '0';
-            dec <= '0';
         elsif synch_a & synch_b = "11" then 
             next_state <= S_2; -- inc
-            inc <= '1';
         elsif synch_a & synch_b = "10" then  
             next_state <= S_reset; -- err
-            err <= '1';
         end if;
       when S_2 =>
         if synch_a & synch_b = "00" then 
             next_state <= S_reset; -- err 
-            err <= '1';
         elsif synch_a & synch_b = "01" then 
             next_state <= S_1; -- dec  
-            err <= '1';
         elsif synch_a & synch_b = "11" then 
             next_state <= S_2; 
-            inc <= '0';
-            dec <= '0';
         elsif synch_a & synch_b = "10" then  
             next_state <= S_3; -- inc
-            inc <= '1';
         end if;
       when S_3 => 
         if synch_a & synch_b = "00" then 
             next_state <= S_0; -- inc
-            inc <= '1';
         elsif synch_a & synch_b = "01" then 
             next_state <= S_reset; -- err
-            err <= '1';
         elsif synch_a & synch_b = "11" then 
             next_state <= S_2;  -- dec  
-            dec <= '1';
         elsif synch_a & synch_b = "10" then  
             next_state <= S_3;
-            inc <= '0';
-            dec <= '0';
             end if;
       end case; 
     end process NEXT_STATE_CL;
 
     OUTPUT_LOGIC: process(all) 
     begin 
+      err <= '0';
+      inc <= '0';
+      dec <= '0';
       case current_state is 
         when S_reset => 
           inc <= '0';
@@ -122,21 +110,45 @@ begin
           inc <= '0'; 
           dec <= '0'; 
         when S_0 => 
-          err <= '1' when synch_a and synch_b;
-          inc <= '1' when not synch_a and synch_b else '0'; 
-          dec <= '1' when synch_a and not synch_b else '0';
+          if current_state /= next_state then
+            err <= '1' when synch_a and synch_b;
+            inc <= '1' when not synch_a and synch_b else '0'; 
+            dec <= '1' when synch_a and not synch_b else '0';
+          elsif current_state = next_state then 
+            err <= '0'; 
+            inc <= '0';
+            dec <= '0';
+          end if;
         when S_1 => 
-          err <= '1' when synch_a and not synch_b; 
-          inc <= '1' when synch_a and synch_b else '0';
-          dec <= '1' when not synch_a and not synch_b else '0';
+          if current_state /= next_state then
+            err <= '1' when synch_a and not synch_b; 
+            inc <= '1' when synch_a and synch_b else '0';
+            dec <= '1' when not synch_a and not synch_b else '0';
+          elsif current_state = next_state then 
+            err <= '0'; 
+            inc <= '0';
+            dec <= '0';
+          end if;
         when S_2 => 
-          err <= '1' when not synch_a and not synch_b; 
-          inc <= '1' when synch_a and not synch_b else '0';
-          dec <= '1' when not synch_a and synch_b else '0';
+          if current_state /= next_state then
+            err <= '1' when not synch_a and not synch_b; 
+            inc <= '1' when synch_a and not synch_b else '0';
+            dec <= '1' when not synch_a and synch_b else '0';
+          elsif current_state = next_state then 
+            err <= '0'; 
+            inc <= '0';
+            dec <= '0';
+          end if;
         when S_3 => 
-          err <= '1' when not synch_a and synch_b; 
-          inc <= '1' when not synch_a and not synch_b else '0';
-          dec <= '1' when synch_a and synch_b else '0';
+          if current_state /= next_state then
+            err <= '1' when not synch_a and synch_b; 
+            inc <= '1' when not synch_a and not synch_b else '0';
+            dec <= '1' when synch_a and synch_b else '0';
+        elsif current_state = next_state then 
+            err <= '0'; 
+            inc <= '0';
+            dec <= '0';
+          end if;
         end case;
     end process OUTPUT_LOGIC;
 
